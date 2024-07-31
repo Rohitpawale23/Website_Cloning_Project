2755a52499e7aa51554350393034060905f7e6f1
Rohit  Pawale <145748300+Rohitpawale23@users.noreply.github.com>
2024-07-31 13:03:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+// Website Clonning - Internship Project on Front-End Deveiopment
+
+// var a =document.querySelector(".right .span");
+var a =document.querySelector("p");
+// a.innerHTML = "No";
+
+
+a.addEventListener("click", function(){
+    a.style.color="violet";
+    a.style.fontWeight=600;
+})
